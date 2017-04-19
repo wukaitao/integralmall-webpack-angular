1bf6ea06972a3d360df6929762fba4b882ad058d
wukaitao2006 <wukaitao2006@163.com>
2017-04-19 18:20:27 +0800

    提交模板结构

--- client/assets/lib/common.js
+++ client/assets/lib/common.js
@@ -1,36 +1,36 @@
 ﻿//接口api
-apiDomain = 'http://member-sit.cignacmb.com/shop/interface';
+window.apiDomain = 'http://member-sit.cignacmb.com/shop/interface';
 
-initPageData = '/initPageData';//首页和我的积分页面初始化接口
-queryScoreStatisticsMonth = '/queryScoreStatisticsMonth';//查询积分统计月份接口
-queryScoreMonthDetail = '/queryScoreMonthDetail';//查询积分月份明细接口
-queryCombox = '/queryCombox';//查询下拉选框数据接口
-unnode = '/';//积分E卡兑换接口
-queryOrderList = '/queryOrderList';//查询积分订单列表接口
-queryOrderDetail = '/queryOrderDetail';//查询订单详情接口
-confirmReceipt = '/confirmReceipt';//确认收货接口
-cancelOrder = '/cancelOrder';//取消订单接口
-queryLogistics = '/queryLogistics';//查看物流接口
-queryScoreChannelStatus = '/queryScoreChannelStatus';//查询赚积分渠道状态接口
-queryFavoriteList = '/queryFavoriteList';//收藏列表
-addFavorite = '/addFavorite';//添加收藏
-delFavorite = '/delFavorite';//删除收藏
-queryCartList = '/queryCartList';//购物车列表
-addCart = '/addCart';//添加购物车
-delCart = '/delCart';//删除购物车
+window.initPageData = '/initPageData';//首页和我的积分页面初始化接口
+window.queryScoreStatisticsMonth = '/queryScoreStatisticsMonth';//查询积分统计月份接口
+window.queryScoreMonthDetail = '/queryScoreMonthDetail';//查询积分月份明细接口
+window.queryCombox = '/queryCombox';//查询下拉选框数据接口
+window.unnode = '/';//积分E卡兑换接口
+window.queryOrderList = '/queryOrderList';//查询积分订单列表接口
+window.queryOrderDetail = '/queryOrderDetail';//查询订单详情接口
+window.confirmReceipt = '/confirmReceipt';//确认收货接口
+window.cancelOrder = '/cancelOrder';//取消订单接口
+window.queryLogistics = '/queryLogistics';//查看物流接口
+window.queryScoreChannelStatus = '/queryScoreChannelStatus';//查询赚积分渠道状态接口
+window.queryFavoriteList = '/queryFavoriteList';//收藏列表
+window.addFavorite = '/addFavorite';//添加收藏
+window.delFavorite = '/delFavorite';//删除收藏
+window.queryCartList = '/queryCartList';//购物车列表
+window.addCart = '/addCart';//添加购物车
+window.delCart = '/delCart';//删除购物车
 
-confirmExchange = '/confirmExchange';//确认兑换
-queryLotteryList = '/queryLotteryList';//抽奖活动专区列表
-doLottery = '/doLottery';//抽奖
-queryRewardList = '/queryRewardList';//中奖客户列表
-queryMyRewardList = '/queryMyRewardList';//我的中奖记录
-getReward = '/getReward';//立即领取
-queryShopList = '/queryShopList';//商品列表
-queryShopInfo = '/queryShopInfo';//查看商品详情
-queryAddressList = '/queryAddressList';//收货地址列表
-updateDefaultAddress = '/updateDefaultAddress';//设置默认地址
-addAddress = '/addAddress';//添加地址
-delAddress = '/delAddress';//删除地址
+window.confirmExchange = '/confirmExchange';//确认兑换
+window.queryLotteryList = '/queryLotteryList';//抽奖活动专区列表
+window.doLottery = '/doLottery';//抽奖
+window.queryRewardList = '/queryRewardList';//中奖客户列表
+window.queryMyRewardList = '/queryMyRewardList';//我的中奖记录
+window.getReward = '/getReward';//立即领取
+window.queryShopList = '/queryShopList';//商品列表
+window.queryShopInfo = '/queryShopInfo';//查看商品详情
+window.queryAddressList = '/queryAddressList';//收货地址列表
+window.updateDefaultAddress = '/updateDefaultAddress';//设置默认地址
+window.addAddress = '/addAddress';//添加地址
+window.delAddress = '/delAddress';//删除地址
 
 //跟踪器
 var trackerList = [];
